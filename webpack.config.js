c98f1bc60e3ccbef4e7c576516ae2d1da1ddf7f3
Soonho Kwon <percy3368@gmail.com>
2023-12-07 00:58:17 +0900

    :wrench: Configure webpack to include tossface assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,57 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
+const path = require("path");
 
 module.exports = (env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
+  mode: argv.mode === "production" ? "production" : "development",
 
   // This is necessary because Figma's 'eval' works differently than normal eval
-  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+  devtool: argv.mode === "production" ? false : "inline-source-map",
 
   entry: {
-    ui: './src/app/index.tsx', // The entry point for your UI code
-    code: './src/plugin/controller.ts', // The entry point for your plugin code
+    ui: "./src/app/index.tsx", // The entry point for your UI code
+    code: "./src/plugin/controller.ts", // The entry point for your plugin code
   },
 
   module: {
     rules: [
       // Converts TypeScript code to JavaScript
-      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      { test: /\.css$/, use: ['style-loader', { loader: 'css-loader' }] },
+      { test: /\.css$/, use: ["style-loader", { loader: "css-loader" }] },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg)$/, loader: 'url-loader' },
+      { test: /\.(png|jpg|gif|webp|svg)$/, loader: "url-loader" },
+
+      // Add tossface svg files as source
+      {
+        test: /\.svg$/,
+        include: path.resolve(__dirname, "tossface/dist/svg"),
+        type: "asset/source",
+      },
     ],
   },
 
   // Webpack tries these extensions for you if you omit the extension like "import './file'"
-  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+    alias: {
+      "@tossface": path.resolve(__dirname, "tossface/dist/svg"),
+    },
+  },
 
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"), // Compile into a folder called "dist"
   },
 
   // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/app/index.html',
-      filename: 'ui.html',
-      chunks: ['ui'],
+      template: "./src/app/index.html",
+      filename: "ui.html",
+      chunks: ["ui"],
       cache: false,
     }),
     new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/]),
