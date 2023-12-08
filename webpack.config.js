4d7a439254333cc0b30347403b2044895f6d149d
Soonho Kwon <percy3368@gmail.com>
2023-12-09 02:34:05 +0900

    :bug: Separate UI assets from tossface assets

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,11 @@ module.exports = (env, argv) => ({
       { test: /\.css$/, use: ["style-loader", { loader: "css-loader" }] },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg)$/, loader: "url-loader" },
+      {
+        test: /\.(png|jpg|gif|webp|svg)$/,
+        loader: "url-loader",
+        exclude: path.resolve(__dirname, "tossface"),
+      },
 
       // Add tossface svg files as source
       {
